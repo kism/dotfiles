92b73c0cff221ceb10169ba73a236ba42a69b021
Kieran Gee <kieran.gee@cogitogroup.net>
2020-06-02 15:37:15 +1000

    Mad hacks to launch as admin if necessary.

--- !others/Windows/Vivaldi/patch.ps1
+++ !others/Windows/Vivaldi/patch.ps1
@@ -2,6 +2,12 @@ $fileToCheck = "$($env:LOCALAPPDATA)\Vivaldi\Application\vivaldi.exe"
 if (Test-Path $fileToCheck -PathType leaf) {
     $vivaldibasepath = "$($env:LOCALAPPDATA)\Vivaldi\Application"
 } else {
+	# Start as admin in this case
+	if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+		$CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+		Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+		Exit
+	}
     $vivaldibasepath = "C:\Program Files (x86)\Vivaldi\Application"
 }
 
