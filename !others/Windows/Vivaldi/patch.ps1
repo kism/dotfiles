813cb5e8893f694c3c1bb749cf1992d89098fc6f
Kieran Gee <kieran.gee@cogitogroup.net>
2020-06-02 12:58:16 +1000

    Add handling for an appdata/local install

--- !others/Windows/Vivaldi/patch.ps1
+++ !others/Windows/Vivaldi/patch.ps1
@@ -1,5 +1,9 @@
-#Requires -RunAsAdministrator
-$vivaldibasepath = "C:\Program Files (x86)\Vivaldi\Application"
+$fileToCheck = "$($env:LOCALAPPDATA)\Vivaldi\Application\vivaldi.exe"
+if (Test-Path $fileToCheck -PathType leaf) {
+    $vivaldibasepath = "$($env:LOCALAPPDATA)\Vivaldi\Application"
+} else {
+    $vivaldibasepath = "C:\Program Files (x86)\Vivaldi\Application"
+}
 
 $FileNames = Get-ChildItem -Path $vivaldibasepath -Directory
 $vivaldibasepath = -join($vivaldibasepath, "\", $FileNames[0].Name, "\resources\vivaldi\style")
