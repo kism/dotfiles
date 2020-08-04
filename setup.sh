fd6cb540274bdf8bf77551049df49faaad4e109b
Kieran Gee <kieran.lost.the.game@gmail.com>
2020-08-04 21:44:09 +1000

    Cleanup, add more messages, dnf and not yum

--- setup.sh
+++ setup.sh
@@ -3,39 +3,50 @@
 baseinstall="vim zsh git htop tmux openssh-server curl" 
 
 function setup_manjaro {
+	hheader "Updating Manjaro"
+	echo
 	sudo pacman -Syyu
+	hheader "Installing Packages"
+	echo
 	sudo pacman -Sy $baseinstall
 	set_shell_chsh
 }
 
 function setup_ubuntu {
+	hheader "Updating Ubuntu"
+	echo
 	sudo apt update
 	sudo apt upgrade -y
+	hheader "Installing Packages"
+	echo
 	sudo apt install -y $baseinstall
 	set_shell_chsh
 }
 
 function setup_centos {
-	sudo yum clean all
-	sudo yum update
-	sudo yum install -y epel-release
-	sudo yum install -y $baseinstall
+	hheader "Updating CentOS"
+	echo
+	sudo dnf clean all
+	sudo dnf update
+	sudo dnf install -y epel-release
+	hheader "Installing Packages"
+	echo
+	sudo dnf install -y $baseinstall
 }
 
 function set_shell_chsh {
 	echo
-	echo "Setting your default shell"
+	hheader "Setting zsh as user shell"
+	header "Setting your default shell:"
 	myshell=$(which zsh)
-	chsh -s $myshell $USER
+	chsh -s $myshell $USER; checksuccess
 }
 
 function checksuccess {
 	if [ $? -eq 0 ]; then
-  		echo "Success!"
-  		echo
+  		echo -e "\033[0;32mSuccess!\033[0m"
 	else
-  		echo "Failure" >&2
-  		exit 1
+  		echo -e "\033[0;31mFailure\033[0m" >&2
 	fi
 }
 
@@ -44,7 +55,6 @@ function hsep {
 	echo "  .--.      .--.      .--.      .--.      .--.      .--.      .--.      .--."
 	echo ":::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\::::::::.\\"
 	echo "'      \`--'      \`--'      \`--'      \`--'      \`--'      \`--'      \`--'      \`"
-	echo
 }
 
 function hheader () {
@@ -52,24 +62,28 @@ function hheader () {
 	echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 	echo " $1"
 	echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+}
+
+function header () {
 	echo
+	echo -e "\033[0;35m$1\033[0m"
 }
 
 hsep
 
-# Start
-echo "Installing packages will require sudo"
-sudo echo
+hheader "Dotfiles Install!"
 
-if [ $? -eq 0 ]; then
-  echo "Starting install!"
-  echo
-else
+# Start
+echo -e "\nInstalling packages will require sudo"
+sudo echo "Starting install!"
+
+if [ $? -ne 0 ]; then
   echo "sudo failed" >&2
   exit 1
 fi
 
 # Call function according to detected distro
+header "Detecting OS:"
 if grep Manjaro /etc/os-release; then
 	setup_manjaro
 elif grep Ubuntu /etc/os-release; then
@@ -78,47 +92,49 @@ elif grep CentOS /etc/os-release; then
 	setup_centos
 else
 	echo "Unknown *Nix distro"
+	echo "Install tmux, vim and zsh"
 fi
-
-hsep
 
 # BASH
 hheader "Setting up bash"
-cp _bash/.bashrc ~/.bashrc
-checksuccess
+header "Copying .bashrc"
+cp _bash/.bashrc ~/.bashrc; checksuccess
 
 # TMUX
 hheader "Setting up tmux"
-cp _tmux/.tmux.conf ~/.tmux.conf
-checksuccess
+header "Copying .tmux.conf"
+cp _tmux/.tmux.conf ~/.tmux.conf; checksuccess
 
 # VIM
 hheader "Setting up vim"
+header "Checking for Vundle:"
 vundlelocation=~/.vim/bundle/Vundle.vim
 if test -d $vundlelocation; then
-	echo "Vundle Found!"
+	echo -e "Vundle Found!"
     git -C $vundlelocation pull
 else
-	echo "Vundle Not Found!"
+	echo -e "Vundle Not Found!"
 	git clone https://github.com/VundleVim/Vundle.vim.git $vundlelocation
 fi
 checksuccess
-cp _vim/.vimrc ~/.vimrc
-checksuccess
+header "Copying .vimrc"
+cp _vim/.vimrc ~/.vimrc; checksuccess
 
-echo "Remember to run :PluginInstall"
+echo -e "\nRemember to run :PluginInstall"
 
 # ZSH
 hheader "Setting up zsh"
 antigenlocation=~/.antigen
 if ! test -d $antigenlocation; then
-	mkdir $antigenlocation
-	checksuccess
+	echo "Creating $antigenlocation"
+	mkdir $antigenlocation; checksuccess
 fi
-curl -L git.io/antigen > ~/.antigen/antigen.zsh
-checksuccess
-cp _zsh/.zshrc ~/.zshrc
-checksuccess
+header "Downloading Antigen:"
+curl -L git.io/antigen > ~/.antigen/antigen.zsh; checksuccess
 
-hheader " Done!"
+header "Copying .vimrc"
+cp _zsh/.zshrc ~/.zshrc; checksuccess
+
+echo -e "\nAll done!"
 hsep
+echo
