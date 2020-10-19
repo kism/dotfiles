95fcd7d96b64205ba179deb62024b2d1197993cb
Kieran Gee <kieran.lost.the.game@gmail.com>
2020-10-20 09:35:39 +1100

    Create generate_bee.sh

---
+++ !others/Linux/sudolecture/generate_bee.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# By Sefa Eyeoglu as far as I can tell
+output="/etc/sudo_lecture"
+
+if [ $# -gt 0 ]; then
+    output="$@"
+fi
+
+echo -e "\e[1m     \e[32m\"Bee\" careful    \e[34m__" > $output
+echo -e "       \e[32iwith sudo!    \e[34m// \\" >> $output
+echo -e "                     \\\\\\\\_/ \e[33m//" >> $output
+echo -e "   \e[35m''-.._.-''-.._.. \e[33m-(||)(')" >> $output
+echo -e "                     '''\e[0m" >> $output
