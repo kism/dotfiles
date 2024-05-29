9c86cee1b912862fe1bbb26650e1e5f577dbb988
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-05-29 10:58:02 +0800

    better logging

--- __others/Windows/Windows_Oh-My-Posh.ps1
+++ __others/Windows/Windows_Oh-My-Posh.ps1
@@ -1,8 +1,8 @@
-Write-Host "Install/Update Oh My Posh"
+Write-Host "🙋‍♀️ Install/Update Oh My Posh"
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
 
-Write-Host "Copy my Powershell profile"
+Write-Host "🙋‍♀️ Copy my Powershell profile"
 Copy-Item .\posh\Microsoft.PowerShell_profile.ps1 -Destination $PROFILE
 
-Write-Host "Copy my Powershell theme"
+Write-Host "🙋‍♀️ Copy my Powershell theme"
 Copy-Item .\posh\ys2.omp.json -Destination $env:POSH_THEMES_PATH/ys2.omp.json
