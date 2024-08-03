fef14f6ce8209d3224b74cd9fd82b87249704857
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-08-03 23:59:02 +0800

    Update _Windows_Presetup.ps1

--- __others/Windows/_Windows_Presetup.ps1
+++ __others/Windows/_Windows_Presetup.ps1
@@ -1,4 +1,6 @@
 Write-Host "Paste these into admin terminal windows specifically"
 
+# https://github.com/ChrisTitusTech/winutil check
+
 # Set powershell script settings
 Set-ExecutionPolicy Unrestricted -Scope CurrentUser
