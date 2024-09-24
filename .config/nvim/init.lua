431860a7507598385dd3a6827cccf8c563cd37a3
Kieran Gee <kieran.gee@telethonkids.org.au>
2024-09-24 15:56:47 +0800

    more config for root user

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,7 +12,7 @@ if user_id ~= "0" then
     vim.call('plug#begin')
 
     --- Basics
-    Plug('tpope/vim-sensible')
+    -- Plug('tpope/vim-sensible')
     Plug('nvim-lualine/lualine.nvim')
     Plug('kyazdani42/nvim-web-devicons')
 
@@ -93,12 +93,112 @@ vim.cmd('set expandtab')
 vim.cmd('set nowrap') --- Wordwrap off
 vim.cmd('set mouse=') --- Mouse off
 vim.cmd('set tw=0') --- vim.cmd('set text wrapping off for the language formatter
-if user_id ~= "0" then
-  vim.cmd('set noshowmode') --- Dont display -- Insert -- since that's handled by ???
-end
+vim.cmd('set noshowmode') --- Dont display -- Insert -- since that's handled by The statusline
 
 vim.cmd('set ttyfast') --- Speed up scrolling in Vim
 
 --- Binds
 vim.cmd('map q <Nop>') --- Unbind macros
 
+--- Statusline (Root user) https://nuxsh.is-a.dev/blog/custom-nvim-statusline.html
+if user_id == "0" then
+    local modes = {
+        ["n"] = "NORMAL",
+        ["no"] = "NORMAL",
+        ["v"] = "VISUAL",
+        ["V"] = "VISUAL LINE",
+        [""] = "VISUAL BLOCK",
+        ["s"] = "SELECT",
+        ["S"] = "SELECT LINE",
+        [""] = "SELECT BLOCK",
+        ["i"] = "INSERT",
+        ["ic"] = "INSERT",
+        ["R"] = "REPLACE",
+        ["Rv"] = "VISUAL REPLACE",
+        ["c"] = "COMMAND",
+        ["cv"] = "VIM EX",
+        ["ce"] = "EX",
+        ["r"] = "PROMPT",
+        ["rm"] = "MOAR",
+        ["r?"] = "CONFIRM",
+        ["!"] = "SHELL",
+        ["t"] = "TERMINAL"
+    }
+
+    local function mode()
+        local current_mode = vim.api.nvim_get_mode().mode
+        return string.format(" %s ", modes[current_mode]):upper()
+    end
+
+    local function update_mode_colors()
+        local current_mode = vim.api.nvim_get_mode().mode
+        local mode_color = "%#StatusLineAccent#"
+        if current_mode == "n" then
+            mode_color = "%#StatuslineAccent#"
+        elseif current_mode == "i" or current_mode == "ic" then
+            mode_color = "%#StatuslineInsertAccent#"
+        elseif current_mode == "v" or current_mode == "V" or current_mode == "" then
+            mode_color = "%#StatuslineVisualAccent#"
+        elseif current_mode == "R" then
+            mode_color = "%#StatuslineReplaceAccent#"
+        elseif current_mode == "c" then
+            mode_color = "%#StatuslineCmdLineAccent#"
+        elseif current_mode == "t" then
+            mode_color = "%#StatuslineTerminalAccent#"
+        end
+        return mode_color
+    end
+
+    local function filepath()
+        local fpath = vim.fn.fnamemodify(vim.fn.expand "%", ":~:.:h")
+        if fpath == "" or fpath == "." then
+            return " "
+        end
+
+        return string.format(" %%<%s/", fpath)
+    end
+
+    local function filename()
+        local fname = vim.fn.expand "%:t"
+        if fname == "" then
+            return ""
+        end
+        return fname .. " "
+    end
+
+    local function filetype()
+        return string.format(" %s ", vim.bo.filetype):upper()
+    end
+
+    local function lineinfo()
+        if vim.bo.filetype == "alpha" then
+            return ""
+        end
+        return " %P %l:%c "
+    end
+
+    Statusline = {}
+
+    Statusline.active = function()
+        return table.concat {"%#Statusline#", update_mode_colors(), mode(), "%#Normal# ", filepath(), filename(),
+                             "%#Normal#", "%=%#StatusLineExtra#", filetype(), lineinfo()}
+    end
+
+    function Statusline.inactive()
+        return " %F"
+    end
+
+    function Statusline.short()
+        return "%#StatusLineNC#   NvimTree"
+    end
+
+    vim.api.nvim_exec([[
+    augroup Statusline
+    au!
+    au WinEnter,BufEnter * setlocal statusline=%!v:lua.Statusline.active()
+    au WinLeave,BufLeave * setlocal statusline=%!v:lua.Statusline.inactive()
+    au WinEnter,BufEnter,FileType NvimTree setlocal statusline=%!v:lua.Statusline.short()
+    augroup END
+    ]], false)
+
+end
