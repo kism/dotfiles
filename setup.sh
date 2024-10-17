5c3672e3310b5d3434fff4e73098c197a31d4188
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-10-17 12:48:23 +0800

    fix

--- setup.sh
+++ setup.sh
@@ -224,8 +224,7 @@ if type zsh >/dev/null; then
     curl -s -L https://raw.githubusercontent.com/zsh-users/antigen/master/bin/antigen.zsh >~/.antigen/antigen.zsh
 
     h2 "Updating Antigen Bundles:"
-    # Run interactive, run commands
-    zsh -i -c "source ~/.antigen/antigen.zsh; antigen update; antigen reset"
+    zsh -c "source ~/.antigen/antigen.zsh; antigen update; antigen reset"
 else
     h3 "zsh not found, skipping"
 fi
