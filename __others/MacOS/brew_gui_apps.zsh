d0d3e5df07a074305a36cf0b7e33fca034644546
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-12-13 09:06:29 +0800

    fix

---
+++ __others/MacOS/brew_gui_apps.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+brew install --cask betterdisplay
+brew install --cask domzilla-caffeine
+brew install --cask cyberduck
+brew install --cask easy-move-plus-resize
+brew install stats
+brew install secretive
+brew install jordanbaird-ice
+brew install --cask wezterm
+brew install --cask mpv
+
+brew install ffmpeg
+brew install yt-dlp
