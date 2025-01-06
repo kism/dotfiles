f37288c43feccf6c9502a78a11514bc2873bcd58
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-01-06 20:08:39 +0800

    more

--- __others/MacOS/library_backup.sh
+++ __others/MacOS/library_backup.sh
@@ -4,8 +4,5 @@ APP_NAME="com.nuebling.mac-mouse-fix"
 mkdir -p Library/Application\ Support/$APP_NAME
 defaults export $HOME/Library/Application\ Support/$APP_NAME/config.plist - > Library/Application\ Support/$APP_NAME/config.plist
 
-# APP_NAME="com.jordanbaird.Ice"
-# mkdir -p Library/Preferences/$APP_NAME
-# defaults export $HOME/Library/Preferences/$APP_NAME.plist - > Library/Preferences/$APP_NAME.plist
 
 
