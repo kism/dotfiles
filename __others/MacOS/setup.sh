757840b3964b8b973fab9e8f8233cf3c6f1382fb
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-01-09 15:29:27 +0800

    firefox

--- __others/MacOS/setup.sh
+++ __others/MacOS/setup.sh
@@ -63,6 +63,11 @@ defaults write org.mozilla.firefox "FirefoxHome" -dict \
     ImproveSuggest -bool "false" \
     Locked -bool "false"
 
+plutil -insert Extensions -dictionary ~/Library/Preferences/org.mozilla.firefox.plist
+plutil -insert Extensions.Install -array ~/Library/Preferences/org.mozilla.firefox.plist
+plutil -insert Extensions.Install.0 -string "https://addons.mozilla.org/firefox/downloads/file/4407804/bitwarden_password_manager-latest.xpi" ~/Library/Preferences/org.mozilla.firefox.plist
+plutil -insert Extensions.Install.1 -string "https://addons.mozilla.org/firefox/downloads/file/4391011/ublock_origin-latest.xpi" ~/Library/Preferences/org.mozilla.firefox.plist
+
 # Edge Policies https://github.com/TommyTran732/Microsoft-Edge-Policies/blob/main/macOS/Managed%20Preferences/com.microsoft.Edge.plist
 defaults write com.microsoft.Edge "DefaultSearchProviderEnabled" -bool "true"
 defaults write com.microsoft.Edge "DefaultSearchProviderName" -string "Google"
