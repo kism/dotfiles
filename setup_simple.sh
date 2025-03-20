6f2e1406239319021ee661b6ef77c50f4a60a3bf
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-03-20 13:52:40 +0800

    better terminfo

--- setup_simple.sh
+++ setup_simple.sh
@@ -6,14 +6,14 @@ set -e
 
 # Bash
 echo Setting up bash...
-curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.bashrc >~/.bashrc
-curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.bash_profile >~/.bash_profile
-curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.bash_aliases >~/.bash_aliases
-curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.inputrc >~/.inputrc
+curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.bashrc -o ~/.bashrc
+curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.bash_profile -o ~/.bash_profile
+curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.bash_aliases -o ~/.bash_aliases
+curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.inputrc -o ~/.inputrc
 
 # Tmux
 echo Setting up tmux...
-curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.tmux.conf >~/.tmux.conf
+curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.tmux.conf -o ~/.tmux.conf
 if [ $EUID -eq 0 ]; then # If this is setting up the root user account, make the tmux bar red
     sed -i 's/green/red/g' ~/.tmux.conf
     sed -i 's/colour10/colour9/g' ~/.tmux.conf
@@ -21,24 +21,31 @@ fi
 
 # Vim
 echo Setting up vim...
-curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.vimrc >~/.vimrc
+curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.vimrc -o ~/.vimrc
 
 # NeoVim
 echo Setting up neovim...
 mkdir -p ~/.config/nvim/
-curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.config/nvim/init.lua >~/.config/nvim/init.lua
+curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.config/nvim/init.lua -o ~/.config/nvim/init.lua
 
 # Htop
 echo Setting up htop...
 mkdir -p ~/.config/htop/
-curl --silent https://raw.githubusercontent.com/kism/dotfiles/main/.config/htop/htoprc >~/.config/htop/htoprc
+curl --silent https://raw.githubusercontent.com/kism/dotfiles/main/.config/htop/htoprc -o ~/.config/htop/htoprc
 
 if [ "$1" == "--gui" ]; then
     echo Setting up ghostty
     mkdir -p ~/.config/ghostty/
-    curl --silent https://raw.githubusercontent.com/kism/dotfiles/main/.config/ghostty/config >~/.config/ghostty/config
+    curl --silent https://raw.githubusercontent.com/kism/dotfiles/main/.config/ghostty/config -o ~/.config/ghostty/config
 fi
 
 # Zsh
 echo Setting up zsh...
-curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.zshrc >~/.zshrc
+curl --silent -L https://raw.githubusercontent.com/kism/dotfiles/main/.zshrc -o ~/.zshrc
+
+# Terminfo
+echo Setting up terminfo...
+mkdir -p ~/.terminfo/g/
+mkdir -p ~/.terminfo/x/
+curl --silent -L https://github.com/kism/dotfiles/raw/refs/heads/main/.terminfo/g/ghostty -o ~/.terminfo/g/ghostty
+curl --silent -L https://github.com/kism/dotfiles/raw/refs/heads/main/.terminfo/x/xterm-ghostty -o ~/.terminfo/x/xterm-ghostty
