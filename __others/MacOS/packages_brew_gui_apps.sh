e51980a5043590d7ea2f3f37131f03d908496168
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-09-18 19:00:45 +0800

    fix

--- __others/MacOS/packages_brew_gui_apps.sh
+++ __others/MacOS/packages_brew_gui_apps.sh
@@ -8,7 +8,6 @@ fi
 brew install --cask betterdisplay
 brew install --cask domzilla-caffeine
 brew install --cask cyberduck
-brew install --cask easy-move-plus-resize
 brew install stats
 brew install secretive
 brew install jordanbaird-ice
