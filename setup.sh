749a9204ba6a8a0a61e2183009c5318cd546b958
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-10-10 11:28:04 +0800

    fix global gitignore

--- setup.sh
+++ setup.sh
@@ -260,7 +260,6 @@ if type git >/dev/null; then
     echo .Trash-* >> ~/.gitignore_global
     echo .fseventsd >> ~/.gitignore_global
     echo .Spotlight-V100 >> ~/.gitignore_global
-    echo core >> ~/.gitignore_global
     echo Thumbs.db >> ~/.gitignore_global
     git config --global core.excludesfile ~/.gitignore_global
 else
