36e0aa9aad8696e513a674dde9d2c0c766d1f12a
Kieran Gee <kieran.lost.the.game@gmail.com>
2026-09-22 08:55:05 +0800

    fx

--- __others/MacOS/defaults/com.felixblome.easy-ethernet-icon.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-defaults write com.felixblome.easy-ethernet-icon selectedOption -string "macOS"
-defaults write com.felixblome.easy-ethernet-icon showConnectionSpeed -bool "false"
